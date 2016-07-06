353990f2f4e320eb66399b77c273f65a81a556cb
Ian Harris <ian+gh@dgby.org>
2016-07-05 18:29:54 -0700

    Initial commit Added initial "ideas" file for fleshing out some thoughts.

--- project2/main.cpp
+++ project2/main.cpp
@@ -1,15 +1,6 @@
-//
-//  main.cpp
-//  project2
-//
-//  Created by Ian Harris on 06/28/16.
-//  Copyright © 2016 Ian Harris. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
+int main() {
   std::cout << "Hello, World!\n";
-    return 0;
+  return 0;
 }
