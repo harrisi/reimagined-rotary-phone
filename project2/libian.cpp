1253eb023091daf53f95bd505501d12c01167a58
Ian Harris <ian+gh@dgby.org>
2016-07-06 14:36:06 -0700

    Start of moving to a lib
    I don't know what all I want to include in this. I would really like to have vectors, hash maps, simple "db," etc. but I don't know how much time I can commit to this currently.

---
+++ project2/libian.cpp
@@ -0,0 +1,19 @@
+//
+//  libian.cpp
+//  project2
+//
+//  Created by Ian Harris on 07/06/16.
+//  Copyright © 2016 Ian Harris. All rights reserved.
+//
+
+#include "libian.hpp"
+
+void getString() {
+//  char res[MAX_STRING_SIZE];
+//  while(!std::cin.get(res, std::numeric_limits<std::streamsize>::max())) {
+//    std::cin.clear();
+//    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+//    std::cout << "Invalid input! Try again: ";
+//  }
+//  return res;
+}
---
+++ project2/libian.hpp
@@ -0,0 +1,21 @@
+//
+//  libian.hpp
+//  project2
+//
+//  Created by Ian Harris on 07/06/16.
+//  Copyright © 2016 Ian Harris. All rights reserved.
+//
+
+#ifndef libian_hpp
+#define libian_hpp
+
+// I don't like the size restriction for this, but it will have to do. Unless I
+// just read in forever until a delimiter is found. But that would get super
+// K&R, which I don't know if I want to do.
+#define MAX_STRING_SIZE 1000
+
+#include <iostream>
+
+void getString();
+
+#endif /* libian_hpp */
