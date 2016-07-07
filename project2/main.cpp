d9ab523847dfce4f07e4e846d835be984f0a9448
Ian Harris <ian+gh@dgby.org>
2016-07-07 00:03:00 -0700

    Adding songs to DB
    Currently super hacky and fragile, unfortunately. I don't like it at all.

--- project2/main.cpp
+++ project2/main.cpp
@@ -6,7 +6,7 @@
 
 // maximum amount of songs possible. I might try increasing this to 50k to mimic
 // Google Play Music's capacity.
-#define MAX_SONG_DB_SIZE 2000
+const int MAX_SONG_DB_SIZE = 2000;
 
 // private is best, but I don't want to deal with that right now.
 struct Song {
@@ -30,8 +30,10 @@ struct Song {
 };
 
 struct SongDB {
+  int items;
   Song songs[MAX_SONG_DB_SIZE];
   void insert(Song);
+  void search();
 };
 
 bool Song::print() const {
@@ -120,16 +122,56 @@ char doMenu() {
   return res;
 }
 
+void getString(char *buf) {
+  while(!std::cin.get(buf, MAX_STRING_SIZE - 1)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Invalid input! Try again: ";
+  }
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int getInt() {
+  int res;
+  while (!(std::cin >> res)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Invalid input! Try again: ";
+  }
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  return res;
+}
+
 void add(SongDB& song_db) {
   char buf[MAX_STRING_SIZE];
+  // Make sure we maintain index correctness.
+  song_db.items++;
+  
   // I think I'll be able to abstract away the reading of information from
   // loadFile to some function ("songFromStream" or the like) which reads in a
   // stream of delimeter separated values from a stringstream and adds a song
   // object to the db passed to it.
   std::cout << "Song title: ";
-  std::cin.get(buf, MAX_STRING_SIZE - 1);
-  //song_db
-  std::cout << "add\n";
+  getString(buf);
+  std::strncpy(song_db.songs[song_db.items].title, buf, MAX_STRING_SIZE);
+  
+  std::cout << "Song artist: ";
+  getString(buf);
+  std::strncpy(song_db.songs[song_db.items].artist, buf, MAX_STRING_SIZE);
+  
+  std::cout << "Song minutes: ";
+  song_db.songs[song_db.items].minutes = getInt();
+  
+  std::cout << "Song seconds: ";
+  song_db.songs[song_db.items].seconds = getInt();
+  
+  std::cout << "Song album: ";
+  getString(buf);
+  std::strncpy(song_db.songs[song_db.items].album, buf, MAX_STRING_SIZE);
+  
+  song_db.songs[song_db.items].isPopulated = true;
 }
 
 void remove() {
@@ -170,7 +212,7 @@ void loadFile(SongDB& song_db, const char delim) {
   char in[MAX_STRING_SIZE];
   std::ifstream f;
   Items fsm = TITLE;
-  int i = 0; // this overwrites from the beginning every time.
+  //int i = 0; // this overwrites from the beginning every time.
   f.open("songs.txt");
   
   if (!f.is_open()) {
@@ -183,7 +225,7 @@ void loadFile(SongDB& song_db, const char delim) {
   // This needs to check for the case of a newline. How to do this will be
   // potentially tricky.
   while (f.get(in, MAX_STRING_SIZE, delim) ||
-         i <= MAX_SONG_DB_SIZE) {
+         song_db.items <= MAX_SONG_DB_SIZE) {
     if (f.eof()) break; // got to EOF, break.
     if(f.peek() == delim) {
       f.get(); // consume delimiter
@@ -195,33 +237,33 @@ void loadFile(SongDB& song_db, const char delim) {
     // title;artist;mm;ss;album
     switch (fsm) {
       case TITLE:
-        std::strncpy(song_db.songs[i].title, in, MAX_STRING_SIZE);
+        std::strncpy(song_db.songs[song_db.items].title, in, MAX_STRING_SIZE);
         fsm = ARTIST;
         break;
         
       case ARTIST:
-        std::strncpy(song_db.songs[i].artist, in, MAX_STRING_SIZE);
+        std::strncpy(song_db.songs[song_db.items].artist, in, MAX_STRING_SIZE);
         fsm = MINUTES;
         break;
         
       case MINUTES:
-        song_db.songs[i].minutes = static_cast<unsigned int>(atoi(in));
+        song_db.songs[song_db.items].minutes = static_cast<unsigned int>(atoi(in));
         fsm = SECONDS;
         break;
         
       case SECONDS:
-        song_db.songs[i].seconds = static_cast<unsigned int>(atoi(in));
+        song_db.songs[song_db.items].seconds = static_cast<unsigned int>(atoi(in));
         fsm = ALBUM;
         break;
         
       case ALBUM:
-        std::strncpy(song_db.songs[i].album, in, MAX_STRING_SIZE);
+        std::strncpy(song_db.songs[song_db.items].album, in, MAX_STRING_SIZE);
         fsm = TITLE;
-        song_db.songs[i].isPopulated = true;
+        song_db.songs[song_db.items].isPopulated = true;
         // XXX: I hate this.
         if (f.peek() == '\n') { // using peek and panicking is reasonable.
           f.get(); // consume newline.
-          i++; // this is the actual tracking of the index which seems silly.
+          song_db.items++; // this is the actual tracking of the index which seems silly.
         } else {
           if (f.peek() == EOF) { // this catches the case where the file doesn't
                                  // contain a newline before EOF.
